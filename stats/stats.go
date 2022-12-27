b5b5589039bc67bd6fe4ece87076929be676069c
Jonathan Essex <Jonathan.Essex@softwareplumbers.com>
2022-12-27 14:51:21 -0500

    separate stats classes  and instruments

---
+++ stats/stats.go
@@ -0,0 +1,206 @@
+package stats
+
+import (
+	"sort"
+	"strings"
+	"sync"
+
+	"github.com/fission-codes/go-car-mirror/errors"
+	"go.uber.org/zap"
+	"golang.org/x/exp/maps"
+
+	golog "github.com/ipfs/go-log/v2"
+)
+
+var log = golog.Logger("go-car-mirror")
+
+// Stats is an interface for recording events.
+type Stats interface {
+	// Log records an event.
+	Log(string)
+	// WithContext returns a new Stats instance that adds a prefix to all events.
+	WithContext(string) Stats
+	// Logger returns a logger that adds a prefix to all events.
+	Logger() *zap.SugaredLogger
+	// Name returns the name of this Stats instance.
+	Name() string
+}
+
+// Context is a Stats implementation that adds a prefix to all events.
+type Context struct {
+	parent Stats
+	name   string
+	logger *zap.SugaredLogger
+}
+
+// Log records an event.
+func (ctx *Context) Log(event string) {
+	ctx.parent.Log(ctx.name + "." + event)
+}
+
+// Logger returns a logger that adds a prefix to all events.
+func (ctx *Context) Logger() *zap.SugaredLogger {
+	return ctx.logger
+}
+
+// Name returns the name of this Stats instance.
+func (ctx *Context) Name() string {
+	return ctx.name
+}
+
+// WithContext returns a new Stats instance that adds a prefix to all events.
+func (ctx *Context) WithContext(name string) Stats {
+	name = ctx.name + "." + name
+	return &Context{
+		parent: ctx,
+		name:   name,
+		logger: ctx.logger.With("for", name),
+	}
+}
+
+// Snapshot is a snapshot of a Stats instance.
+type Snapshot struct {
+	values map[string]uint64
+}
+
+// Count returns the number of times the given event has been recorded.
+func (snap *Snapshot) Count(event string) uint64 {
+	return snap.values[event]
+}
+
+// Keys returns a sorted list of all events that have been recorded.
+func (snap *Snapshot) Keys() []string {
+	return maps.Keys(snap.values)
+}
+
+// Diff returns a new Snapshot that contains the difference between this Snapshot and the given Snapshot.
+func (snap *Snapshot) Diff(other *Snapshot) *Snapshot {
+	result := make(map[string]uint64)
+	for key, value := range snap.values {
+		result[key] = value
+	}
+	for key, otherValue := range other.values {
+		snapValue := result[key]
+		if snapValue > otherValue {
+			result[key] = snapValue - otherValue
+		} else {
+			result[key] = otherValue - snapValue
+		}
+	}
+	return &Snapshot{values: result}
+}
+
+// Filter returns a new Snapshot that contains only the events that match the given prefix.
+func (snap *Snapshot) Filter(prefix string) *Snapshot {
+	result := make(map[string]uint64)
+	for key, value := range snap.values {
+		if strings.HasPrefix(key, prefix) {
+			result[key] = value
+		}
+	}
+	return &Snapshot{values: result}
+}
+
+// Write writes the snapshot to the given logger.
+func (snap *Snapshot) Write(log *zap.SugaredLogger) {
+	keys := maps.Keys(snap.values)
+	sort.Strings(keys)
+	for _, key := range keys {
+		log.Infow("snapshot", "event", key, "count", snap.values[key])
+	}
+}
+
+// Reporting is an interface for reporting stats.
+type Reporting interface {
+	Count(string) uint64
+	Snapshot() *Snapshot
+}
+
+// DefaultStatsAndReporting is a Stats and Reporting implementation that records events in memory.
+type DefaultStatsAndReporting struct {
+	mutex  sync.RWMutex
+	values map[string]uint64
+	logger *zap.SugaredLogger
+}
+
+// NewDefaultStatsAndReporting returns a new DefaultStatsAndReporting instance.
+func NewDefaultStatsAndReporting() *DefaultStatsAndReporting {
+	return &DefaultStatsAndReporting{
+		mutex:  sync.RWMutex{},
+		values: make(map[string]uint64),
+		logger: &log.SugaredLogger,
+	}
+}
+
+// Log records an event.
+func (ds *DefaultStatsAndReporting) Log(event string) {
+	ds.mutex.Lock()
+	ds.values[event]++
+	ds.mutex.Unlock()
+}
+
+// WithContext returns a new Context instance that adds a prefix to all events.
+func (ds *DefaultStatsAndReporting) WithContext(name string) Stats {
+	return &Context{
+		parent: ds,
+		name:   name,
+		logger: ds.logger.With("for", name),
+	}
+}
+
+// Logger returns a logger that adds a prefix to all events.
+func (ds *DefaultStatsAndReporting) Logger() *zap.SugaredLogger {
+	return ds.logger
+}
+
+// Name returns the name of this DefaultStatsAndReporting instance.
+func (ds *DefaultStatsAndReporting) Name() string {
+	return "root"
+}
+
+// Count returns the number of times the given event has been recorded.
+func (ds *DefaultStatsAndReporting) Count(event string) uint64 {
+	ds.mutex.RLock()
+	defer ds.mutex.RUnlock()
+	return ds.values[event]
+}
+
+// Snapshot returns a snapshot of the current DefaultStatsAndReporting instance.
+func (ds *DefaultStatsAndReporting) Snapshot() *Snapshot {
+	ds.mutex.RLock()
+	defer ds.mutex.RUnlock()
+	values := make(map[string]uint64)
+	for key, value := range ds.values {
+		values[key] = value
+	}
+	return &Snapshot{
+		values: values,
+	}
+}
+
+// Global stats and reporting instances.
+var (
+	GLOBAL_STATS     Stats     = nil
+	GLOBAL_REPORTING Reporting = nil
+)
+
+var initMutex sync.Mutex = sync.Mutex{}
+
+// Init initializes the global stats and reporting instances.
+func Init(stats Stats, reporting Reporting) error {
+	initMutex.Lock()
+	defer initMutex.Unlock()
+	if GLOBAL_STATS == nil && GLOBAL_REPORTING == nil {
+		GLOBAL_STATS = stats
+		GLOBAL_REPORTING = reporting
+		return nil
+	} else {
+		return errors.ErrStatsAlreadyInitialized
+	}
+}
+
+// InitDefault initializes the global stats and reporting instances with a DefaultStatsAndReporting instance.
+func InitDefault() error {
+	defaultStats := NewDefaultStatsAndReporting()
+	return Init(defaultStats, defaultStats)
+}
