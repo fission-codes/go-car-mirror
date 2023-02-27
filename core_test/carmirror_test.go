f99befc8e526d326d8bce9a6e7b42763e44a72a8
Justin Johnson <justin@justinjohnson.io>
2023-02-27 13:55:15 -0600

    WIP

--- core_test/carmirror_test.go
+++ core_test/carmirror_test.go
@@ -132,7 +132,6 @@ type StatusChannel struct {
 }
 
 func (ch *StatusChannel) SendStatus(state batch.BatchState, have filter.Filter[mock.BlockId], want []mock.BlockId) error {
-	log.Debugw("entering", "object", "StatusChannel", "method", "SendStatus", "state", state, "have", have, "want", want)
 	var message *messages.StatusMessage[mock.BlockId, *mock.BlockId, batch.BatchState]
 	if ch.rate > 0 || ch.latency > 0 {
 		message = messages.NewStatusMessage(state, have, want)
@@ -215,7 +214,7 @@ func (sn *MockStatusSender) Close() error {
 
 // http tests did this, but it is right?
 
-func MockBatchTransfer(senderStore *mock.Store, receiverStore *mock.Store, root mock.BlockId, maxBatchSize uint32, bytesPerMs int64, latencyMs int64) error {
+func MockBatchTransferSend(senderStore *mock.Store, receiverStore *mock.Store, root mock.BlockId, maxBatchSize uint32, bytesPerMs int64, latencyMs int64) error {
 
 	snapshotBefore := stats.GLOBAL_REPORTING.Snapshot()
 
@@ -226,9 +225,7 @@ func MockBatchTransfer(senderStore *mock.Store, receiverStore *mock.Store, root 
 		Instrument:    instrumented.INSTRUMENT_STORE | instrumented.INSTRUMENT_ORCHESTRATOR,
 	}
 
-	// No batch status sender to start with
 	sinkResponder := batch.NewSinkResponder[mock.BlockId](receiverStore, config, nil)
-	// sourceResponder := batch.NewSourceResponder[mock.BlockId](senderStore, config, nil, maxBatchSize)
 
 	blockChannel := BlockChannel{
 		make(chan *messages.BlocksMessage[mock.BlockId, *mock.BlockId, batch.BatchState]),
@@ -253,23 +250,6 @@ func MockBatchTransfer(senderStore *mock.Store, receiverStore *mock.Store, root 
 
 	log.Debugf("created senderSession")
 
-	// sinkResponder.SinkConnection(SESSION_ID)
-
-	// sinkConnection := batch.NewGenericBatchSinkConnection[mock.BlockId](stats.GLOBAL_STATS, instrumented.INSTRUMENT_ORCHESTRATOR|instrumented.INSTRUMENT_STORE)
-
-	// TODO: connect the receiver session of the sink responder to the block channel
-	// block channel should be writing blocks to a channel that then results in calls to HandleList(blocks) on the sinkReponder's blockReceiver
-
-	// receiverSession := sinkConnection.Session(
-	// 	NewSynchronizedBlockStore[mock.BlockId](NewSynchronizedBlockStore[mock.BlockId](receiverStore)),
-	// 	NewSimpleStatusAccumulator[mock.BlockId](filter.NewSynchronizedFilter(makeBloom(1024))),
-	// )
-
-	// log.Debugf("created receiverSession")
-
-	// TODO: Wrap blockChannel in a GenericRequestBatchBlockSender
-	// requestBlockChannel := batch.NewGenericRequestBatchBlockSender[mock.BlockId](&blockChannel)
-	// blockSender := sourceConnection.Sender(requestBlockChannel, uint32(maxBatchSize))
 	blockSender := sourceConnection.Sender(&blockChannel, uint32(maxBatchSize))
 
 	sinkResponder.SetBatchStatusSender(&statusChannel)
@@ -278,11 +258,8 @@ func MockBatchTransfer(senderStore *mock.Store, receiverStore *mock.Store, root 
 		return sourceConnection.Receiver(senderSession)
 	})
 
-	// will need to be able to get connection, receiver, session all based on sessionId (hardcoded for test),
-	// all within SinkResponder, such that this call is never needed.
-	// blockChannel.SetBlockListener(sinkConnection.Receiver(receiverSession))
-
 	blockChannel.SetReceiverFunc(func() batch.BatchBlockReceiver[mock.BlockId] {
+		// This will start up a new sink session whenever needed, like if the previously running session's Run method already returned.
 		return sinkResponder.Receiver(SESSION_ID)
 	})
 
@@ -304,8 +281,6 @@ func MockBatchTransfer(senderStore *mock.Store, receiverStore *mock.Store, root 
 
 	go func() {
 		log.Debugf("status listener started")
-		// Will get a value when the channel is closed
-		// TODO: hanging here...
 		err := statusChannel.listen()
 		if err != nil {
 			log.Debugf("status listener error: %s", err)
@@ -321,7 +296,6 @@ func MockBatchTransfer(senderStore *mock.Store, receiverStore *mock.Store, root 
 
 		log.Debugf("sender session started")
 		senderSession.Run(blockSender)
-
 	}()
 
 	// Wait for session to start
@@ -332,7 +306,7 @@ func MockBatchTransfer(senderStore *mock.Store, receiverStore *mock.Store, root 
 		time.Sleep(20 * time.Second)
 		log.Debugf("timeout elapsed")
 
-		// See what goroutine is hung
+		// If we timeout, see what goroutines are hung
 		pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
 
 		if err := senderSession.Cancel(); err != nil {
@@ -343,7 +317,6 @@ func MockBatchTransfer(senderStore *mock.Store, receiverStore *mock.Store, root 
 		}
 	}()
 
-	// Hanging here.  What marks sender as done?
 	if err := <-senderSession.Done(); err != nil {
 		log.Debugf("sender session failed: %v", err)
 	}
@@ -353,28 +326,132 @@ func MockBatchTransfer(senderStore *mock.Store, receiverStore *mock.Store, root 
 		log.Errorf("error closing block sender: %v", err)
 	}
 
-	// TODO: Fix sink session termination and remove the explicit close.
-	// receiverSession.Close()
-
-	// TODO: Technically, in the future this will be multiple sessions, or multiple runs and returns.
-	// So that will mean multiple Done() calls.
-	// Really we just need to make sure that each session is done before we close the channel.
-
-	// Wait for the sessions to close
 	// TODO: Replace with waiting for all channels on the sinkResponder's sessions to be done
 	// If we get here, the sender session is done, so this will get the last living sink session's status
-
-	// I think this is having trouble.
 
 	if err := <-sinkResponder.SinkSession(SESSION_ID).Done(); err != nil {
 		log.Debugf("receiver session failed: %v", err)
 	}
 	log.Debugf("receiver session terminated")
 
-	// TODO: Close status senders for each responder?
-	// if err := statusSender.Close(); err != nil {
-	// 	log.Errorf("error closing status sender: %v", err)
-	// }
+	snapshotAfter := stats.GLOBAL_REPORTING.Snapshot()
+	diff := snapshotBefore.Diff(snapshotAfter)
+	diff.Write(&log.SugaredLogger)
+	return nil
+}
+
+func MockBatchTransferReceive(sinkStore *mock.Store, sourceStore *mock.Store, root mock.BlockId, maxBatchSize uint32, bytesPerMs int64, latencyMs int64) error {
+
+	snapshotBefore := stats.GLOBAL_REPORTING.Snapshot()
+
+	config := batch.Config{
+		MaxBatchSize:  maxBatchSize,
+		BloomCapacity: 1024, // matches value used below before my changes
+		BloomFunction: MOCK_ID_HASH,
+		Instrument:    instrumented.INSTRUMENT_STORE | instrumented.INSTRUMENT_ORCHESTRATOR,
+	}
+
+	sourceResponder := batch.NewSourceResponder[mock.BlockId](sourceStore, config, nil, maxBatchSize)
+
+	blockChannel := BlockChannel{
+		make(chan *messages.BlocksMessage[mock.BlockId, *mock.BlockId, batch.BatchState]),
+		nil,
+		bytesPerMs,
+		latencyMs,
+	}
+
+	statusChannel := StatusChannel{
+		make(chan *messages.StatusMessage[mock.BlockId, *mock.BlockId, batch.BatchState]),
+		nil,
+		bytesPerMs,
+		latencyMs,
+	}
+
+	// The sink is driving for pull
+	sinkConnection := batch.NewGenericBatchSinkConnection[mock.BlockId](stats.GLOBAL_STATS, instrumented.INSTRUMENT_ORCHESTRATOR|instrumented.INSTRUMENT_STORE)
+
+	receiverSession := sinkConnection.Session(
+		NewSynchronizedBlockStore[mock.BlockId](NewSynchronizedBlockStore[mock.BlockId](sinkStore)),
+		NewSimpleStatusAccumulator[mock.BlockId](filter.NewSynchronizedFilter(batch.NewBloomAllocator[mock.BlockId](&config)())),
+	)
+
+	statusSender := sinkConnection.Sender(&statusChannel)
+
+	sourceResponder.SetBatchBlockSender(&blockChannel)
+
+	blockChannel.SetReceiverFunc(func() batch.BatchBlockReceiver[mock.BlockId] {
+		return sinkConnection.Receiver(receiverSession)
+	})
+
+	statusChannel.SetReceiverFunc(func() *batch.SimpleBatchStatusReceiver[mock.BlockId] {
+		return sourceResponder.Receiver(SESSION_ID)
+	})
+
+	log.Debugf("created senders and channels")
+
+	log.Debugf("starting goroutines")
+
+	go func() {
+		log.Debugf("block listener started")
+		err := blockChannel.listen()
+		if err != nil {
+			log.Debugf("block listener error: %s", err)
+		} else {
+			log.Debugf("block listener terminated")
+		}
+	}()
+
+	go func() {
+		log.Debugf("status listener started")
+		err := statusChannel.listen()
+		if err != nil {
+			log.Debugf("status listener error: %s", err)
+		} else {
+			log.Debugf("status listener terminated")
+		}
+	}()
+
+	go func() {
+		log.Debugf("before enqueue")
+		receiverSession.Enqueue(root)
+		log.Debugf("after enqueue")
+
+		log.Debugf("receiver session started")
+		receiverSession.Run(statusSender)
+	}()
+
+	// Wait for session to start
+	<-receiverSession.Started()
+
+	go func() {
+		log.Debugf("timeout started")
+		time.Sleep(20 * time.Second)
+		log.Debugf("timeout elapsed")
+
+		// See what goroutine is hung
+		pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
+
+		if err := receiverSession.Cancel(); err != nil {
+			log.Errorf("error cancelling receiver session: %v", err)
+		}
+		if err := sourceResponder.SourceSession(SESSION_ID).Cancel(); err != nil {
+			log.Errorf("error cancelling sender session: %v", err)
+		}
+	}()
+
+	if err := <-receiverSession.Done(); err != nil {
+		log.Debugf("receiver session failed: %v", err)
+	}
+	log.Debugf("receiver session terminated")
+
+	if err := statusSender.Close(); err != nil {
+		log.Errorf("error closing status sender: %v", err)
+	}
+
+	if err := <-sourceResponder.SourceSession(SESSION_ID).Done(); err != nil {
+		log.Debugf("sender session failed: %v", err)
+	}
+	log.Debugf("sender session terminated")
 
 	snapshotAfter := stats.GLOBAL_REPORTING.Snapshot()
 	diff := snapshotBefore.Diff(snapshotAfter)
@@ -387,8 +464,20 @@ func TestMockTransferToEmptyStoreSingleBatchNoDelay(t *testing.T) {
 	root := mock.AddRandomTree(context.Background(), senderStore, 10, 5, 0.0)
 	receiverStore := mock.NewStore(mock.DefaultConfig())
 
-	MockBatchTransfer(senderStore, receiverStore, root, 5000, 0, 0)
+	MockBatchTransferSend(senderStore, receiverStore, root, 5000, 0, 0)
 	if !receiverStore.HasAll(root) {
+		t.Errorf("Expected receiver store to have all nodes")
+	}
+}
+
+func TestMockTransferToEmptyStoreSingleBatchNoDelayReceive(t *testing.T) {
+	t.Skip("Skipping test")
+	sourceStore := mock.NewStore(mock.DefaultConfig())
+	root := mock.AddRandomTree(context.Background(), sourceStore, 10, 5, 0.0)
+	sinkStore := mock.NewStore(mock.DefaultConfig())
+
+	MockBatchTransferReceive(sinkStore, sourceStore, root, 5000, 0, 0)
+	if !sinkStore.HasAll(root) {
 		t.Errorf("Expected receiver store to have all nodes")
 	}
 }
@@ -399,7 +488,7 @@ func TestMockTransferToEmptyStoreSingleBatchDelayed(t *testing.T) {
 	receiverStore := mock.NewStore(mock.DefaultConfig())
 	receiverStore.Reconfigure(blockStoreConfig)
 	senderStore.Reconfigure(blockStoreConfig)
-	MockBatchTransfer(senderStore, receiverStore, root, 5000, GBIT_SECOND, TYPICAL_LATENCY)
+	MockBatchTransferSend(senderStore, receiverStore, root, 5000, GBIT_SECOND, TYPICAL_LATENCY)
 	if !receiverStore.HasAll(root) {
 		t.Errorf("Expected receiver store to have all nodes")
 	}
@@ -410,7 +499,7 @@ func TestMockTransferToEmptyStoreMultiBatchNoDelay(t *testing.T) {
 	senderStore := mock.NewStore(mock.DefaultConfig())
 	root := mock.AddRandomTree(context.Background(), senderStore, 10, 5, 0.0)
 	receiverStore := mock.NewStore(mock.DefaultConfig())
-	MockBatchTransfer(senderStore, receiverStore, root, 50, 0, 0)
+	MockBatchTransferSend(senderStore, receiverStore, root, 50, 0, 0)
 	if !receiverStore.HasAll(root) {
 		t.Errorf("Expected receiver store to have all nodes")
 	}
@@ -423,7 +512,7 @@ func TestMockTransferToEmptyStoreMultiBatchDelayed(t *testing.T) {
 	receiverStore := mock.NewStore(mock.DefaultConfig())
 	receiverStore.Reconfigure(blockStoreConfig)
 	senderStore.Reconfigure(blockStoreConfig)
-	MockBatchTransfer(senderStore, receiverStore, root, 50, GBIT_SECOND, TYPICAL_LATENCY)
+	MockBatchTransferSend(senderStore, receiverStore, root, 50, GBIT_SECOND, TYPICAL_LATENCY)
 	if !receiverStore.HasAll(root) {
 		t.Errorf("Expected receiver store to have all nodes")
 	}
@@ -439,7 +528,7 @@ func TestMockTransferSingleMissingBlockNoDelay(t *testing.T) {
 		t.Errorf("Could not find random block %v", err)
 	}
 	receiverStore.Remove(block.Id())
-	MockBatchTransfer(senderStore, receiverStore, root, 10, 0, 0)
+	MockBatchTransferSend(senderStore, receiverStore, root, 10, 0, 0)
 	if !receiverStore.HasAll(root) {
 		t.Errorf("Expected receiver store to have all nodes")
 	}
@@ -458,7 +547,7 @@ func TestMockTransferSingleMissingBlockDelayed(t *testing.T) {
 	receiverStore.Remove(block.Id())
 	receiverStore.Reconfigure(blockStoreConfig)
 	senderStore.Reconfigure(blockStoreConfig)
-	MockBatchTransfer(senderStore, receiverStore, root, 10, GBIT_SECOND, TYPICAL_LATENCY)
+	MockBatchTransferSend(senderStore, receiverStore, root, 10, GBIT_SECOND, TYPICAL_LATENCY)
 	if !receiverStore.HasAll(root) {
 		t.Errorf("Expected receiver store to have all nodes")
 	}
@@ -470,7 +559,7 @@ func TestMockTransferSingleMissingTreeNoDelay(t *testing.T) {
 	receiverStore := mock.NewStore(mock.DefaultConfig())
 	receiverStore.AddAll(context.Background(), senderStore)
 	root := mock.AddRandomTree(context.Background(), senderStore, 10, 5, 0.1)
-	MockBatchTransfer(senderStore, receiverStore, root, 50, 0, 0)
+	MockBatchTransferSend(senderStore, receiverStore, root, 50, 0, 0)
 	if !receiverStore.HasAll(root) {
 		t.Errorf("Expected receiver store to have all nodes")
 		receiverStore.Dump(root, &log.SugaredLogger, "")
@@ -486,7 +575,7 @@ func TestMockTransferSingleMissingTreeDelayed(t *testing.T) {
 	root := mock.AddRandomTree(context.Background(), senderStore, 10, 5, 0.1)
 	receiverStore.Reconfigure(blockStoreConfig)
 	senderStore.Reconfigure(blockStoreConfig)
-	MockBatchTransfer(senderStore, receiverStore, root, 50, GBIT_SECOND, TYPICAL_LATENCY)
+	MockBatchTransferSend(senderStore, receiverStore, root, 50, GBIT_SECOND, TYPICAL_LATENCY)
 	if !receiverStore.HasAll(root) {
 		t.Errorf("Expected receiver store to have all nodes")
 		receiverStore.Dump(root, &log.SugaredLogger, "")
